7fd4f9c4cdcddfe3175f50f697ef0aa8026aede5
delmas <azertyazerty1ze@gmail.com>
2023-10-31 23:39:25 +0000

    login

--- src/Components/HeaderAndFooter/Header.js
+++ src/Components/HeaderAndFooter/Header.js
@@ -1,46 +1,42 @@
 import {Link} from "react-router-dom";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faSignOutAlt, faUserDoctor} from "@fortawesome/free-solid-svg-icons";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Button} from "@nextui-org/react";
+
 
 
 
 export const Header = () => {
     return (
         <>
-            <nav className=" navbar navbar-expand-lg bg-light py-3 ">
-                <div className="container-fluid">
-                    <Link className="navbar-brand ms-2" to={"#"}>
-                        <FontAwesomeIcon icon={faUserDoctor} style={{color: "#198754",height:"80 px",width:"80"}} />
-                    </Link>
-                    <button className="navbar-toggler" type="button"  data-bs-toggle="collapse"
-                            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                            aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <Link className=" btn btn-outline-success mx-2 " aria-current="page" to={"#"}>Consultation</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className=" btn btn-outline-success mx-2  " aria-current="page" to={"#"}>Dossier patient</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className=" btn btn-outline-success mx-2 " aria-current="page" to={"#"}>Voir dossier patient</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className=" btn btn-outline-success mx-2 " aria-current="page" to={"#"}>Ajouter dossier</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className=" btn btn-outline-success  " aria-current="page" to={"#"}>Modifier dossier</Link>
-                            </li>
-                        </ul>
-                        <Link className="icon-link me-5" to={"#"} >
-                            <FontAwesomeIcon icon={faSignOutAlt} style={{color: "#1f5142",}} />
+            <Navbar className="bg-[#a5e194] relative ">
+                <NavbarBrand >
+                    {/*<AcmeLogo />*/}
+                    <p className="font-bold text-slate-50">ACME</p>
+                </NavbarBrand>
+                <NavbarContent className="hidden sm:flex gap-4" justify="center">
+                    <NavbarItem>
+                        <Link className={"text-slate-50"}  to={"#"}>
+                            Features
                         </Link>
-                    </div>
-                </div>
-            </nav>
+                    </NavbarItem>
+                    <NavbarItem >
+                        <Link to={"#"} className={"text-slate-50"} aria-current="page">
+                            Customers
+                        </Link>
+                    </NavbarItem>
+                    <NavbarItem>
+                        <Link className={"text-slate-50"} to={"#"}>
+                            Integrations
+                        </Link>
+                    </NavbarItem>
+                </NavbarContent>
+                <NavbarContent justify="end">
+                    <NavbarItem>
+                        <Button  className={"text-slate-50"} href="#" variant="flat">
+                            Sign Up
+                        </Button>
+                    </NavbarItem>
+                </NavbarContent>
+            </Navbar>
 
         </>
     )
