7a1ca3225b1ce15b92d46c6d2e542020a31f8e64
delmas <azertyazerty1ze@gmail.com>
2023-11-01 22:45:52 +0000

    login

--- src/Components/Utilisateur/Login.js
+++ src/Components/Utilisateur/Login.js
@@ -1,30 +1,43 @@
 import {AuthenticationContext} from "../../Context/Context";
 import {useContext} from "react";
 import {useForm} from "react-hook-form";
+import {useNavigate} from "react-router-dom";
+import {utilisateurApi} from "../../Api/ApiUtilisateur";
 
 
 const Login = () => {
     const [authState,setAuthState] = useContext(AuthenticationContext);
     const {register,handleSubmit,formState:{errors}}
-        =useForm({defaultValues:{}})
+        =useForm({defaultValues:{id:"",password:""}})
+    const navigate =useNavigate()
+
+    const handleLogin = (dataForm)=>{
+        utilisateurApi().connexionUtilisateur()
+    }
     return (
         <div className="">
             <div className=" flex flex-col justify-center min-h-screen ">
             <div className="w-full p-6 m-auto bg-white rounded-md shadow-md ring-2 ring-[#a5e194] lg:max-w-lg">
                 <h1 className="text-3xl font-semibold text-center text-[#a5e194]">Connexion</h1>
-                <form className="space-y-4">
+                <form className="space-y-4" onSubmit={handleSubmit(handleLogin)}>
                     <div>
                         <label className="label">
-                            <span className="text-base text-[#a5e194] label-text">Email</span>
+                            <span className="text-base text-[#a5e194] label-text">Username</span>
                         </label>
-                        <input type="text" placeholder="Email Address" className="w-full bg-[#a5e194] input input-bordered" />
+                        <input type="text" placeholder="Email Address" className="w-full bg-[#a5e194] input input-bordered"
+                               {...register("id",{required:'username est obligatoire'})}
+                        />
+                        <span className={"text-danger"}>{errors.id?.message}</span>
                     </div>
                     <div>
                         <label className="label">
                             <span className="text-base text-[#a5e194] label-text">Password</span>
                         </label>
                         <input type="password"  placeholder="Enter Password"
-                               className="w-full input input-bordered bg-[#a5e194]" />
+                               className="w-full input input-bordered bg-[#a5e194]"
+                               {...register("password",{required:'password est obligatoire'})}
+                        />
+                        <span className={"text-danger"}>{errors.password?.message}</span>
                     </div>
                     <div>
                         <button className="btn btn-block border-[#a5e194] text-[#a5e194] bg-slate-50 mt-5">Login</button>
