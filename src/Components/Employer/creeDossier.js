746896903cd1310161660c5f56685f6bb4e407c5
delmas <azertyazerty1ze@gmail.com>
2023-11-11 21:08:16 +0000

    creeDossier.js

--- src/Components/Employer/creeDossier.js
+++ src/Components/Employer/creeDossier.js
@@ -30,45 +30,55 @@ const CreeDossier = () => {
 
     return (
         <>
-            <section className="max-w-4xl p-6 mx-auto bg-[#a5e194] rounded-md shadow-md dark:bg-[#a5e194] my-20 mb-[320px]">
+            <section className="max-w-4xl p-6 mx-auto bg-[#a5e194] rounded-md shadow-md dark:bg-[#a5e194] my-20 mb-[280px]">
                 <h1 className="text-xl font-bold text-white capitalize dark:text-white">Dossier Patient</h1>
-                <form onClick={handleSubmit(submit)}>
-                    <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+                <form onSubmit={handleSubmit(submit)}>
+                    <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-3">
                         <div>
                             <label className="text-white dark:text-gray-200" form="username">identifiant</label>
                             <input id="username" type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
                                    {...register("id", {required:"veiller rentrer un identifiant"})}/>
+                            <span className={"text-danger"}>
+                            {errors.id?.message}
+                          </span>
                         </div>
                         <div>
                             <label className="text-white dark:text-gray-200" form="username">Ville</label>
                             <input id="username" type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
                                    {...register("ville", {required:"veiller rentrer une ville"})}/>
+                            <span className={"text-danger"}>
+                            {errors.ville?.message}
+                          </span>
                         </div>
+
                         <div>
                             <label className="text-white dark:text-gray-200" form="emailAddress">Age</label>
                             <input id="emailAddress" type="number" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
                                    {...register("age", {required:"veiller rentrer l'age"})}/>
+                            <span className={"text-danger"}>
+                            {errors.age?.message}
+                          </span>
                         </div>
                         <div className="form-control h-full">
                             <label className="label cursor-pointer">
                                 <span className="label-text">Homme</span>
-                                <input type="radio" name="radio-10"  className="radio checked:bg-[#a5e194] " defaultChecked={true}
-                                    {...register("masculin")}/>
                             </label>
+                            <input type="radio" name="radio-8" className="radio radio-[#a5e194] checked:bg-[#a5e194]"
+                                   {...register("masculin")}/>
                         </div>
                         <div className="form-control">
                             <label className="label cursor-pointer">
                                 <span className="label-text">Femme</span>
-                                <input type="radio" name="radio-10" className="radio checked:bg-[#a5e194]"
-                                    {...register("feminin")}/>
                             </label>
+                            <input type="radio" name="radio-8" className="radio radio-[#a5e194] checked:bg-[#a5e194]"
+                                   {...register("masculin")}/>
                         </div>
                         <div className="form-control">
                             <label className="label cursor-pointer">
                                 <span className="label-text">Femme enceinte</span>
-                                <input type="radio" name="radio-10" className="radio checked:bg-[#a5e194]"
-                                    {...register("enceinte")}/>
                             </label>
+                            <input type="radio" name="radio-8" className="radio radio-[#a5e194]  checked:bg-[#a5e194]"
+                                   {...register("masculin")}/>
                         </div>
 
                     </div>
