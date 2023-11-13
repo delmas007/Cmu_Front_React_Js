f820351f889700433c043554ac33119e7341e4d8
delmas007 <azertyazerty1ze@gmail.com>
2023-11-13 12:30:37 +0100

    SupprimerDossier.js

--- src/Components/Employer/SupprimerDossier.js
+++ src/Components/Employer/SupprimerDossier.js
@@ -1,4 +1,31 @@
+import {useForm} from "react-hook-form";
+import {useContext, useState} from "react";
+import {AuthenticationContext} from "../../Context/Context";
+import {useHttpClient} from "../../Api/Interceptors";
+import {employerApi} from "../../Api/ApiEmployer";
+
 const SupprimerDossier = () =>{
+    const {register,handleSubmit,formState:{errors}}=useForm()
+    const [authState,setAuthState] = useContext(AuthenticationContext);
+    let httpClient = useHttpClient(authState.token)
+    const dossier = employerApi(httpClient);
+    const [alertState, setAlertState] = useState({
+        show : false,
+        title:"",
+        close : handleCloseModal
+    });
+    const supp = (dataForm)=>{
+        dossier.supprimerDossier(dataForm)
+            .then(resp=>{
+                setAlertState({
+                    ...alertState, show: true,
+                    title: "Supprimer dossier"
+                })
+            })
+    }
+    function handleCloseModal(){
+        setAlertState({...alertState, show: false});
+    }
     return (
         <>
             <div className={" fex flex-col mb-[518px]"}>
@@ -7,7 +34,7 @@ const SupprimerDossier = () =>{
                 </div>
 
                 <div className={"w-full flex justify-center mt-20"}>
-                    <form className="w-full max-w-sm">
+                    <form className="w-full max-w-sm" onSubmit={handleSubmit(supp)}>
                         <div className="md:flex md:items-center mb-6">
                             <div className="md:w-1/3">
                                 <label className="block text-[#a5e194] font-bold md:text-right mb-1 md:mb-0 pr-4"
@@ -18,8 +45,12 @@ const SupprimerDossier = () =>{
                             <div className="md:w-[700px]">
                                 <input
                                     className="bg-gray-200 appearance-none border-2 border-[#dddada] rounded w-full py-2 px-4 text-[#a5e194] leading-tight focus:outline-none focus:bg-white focus:border-[#a5e194]"
-                                    id="inline-full-name" type="text" placeholder={"52bfd7cf-a451-4779-8b10-b47db12cdc3f"}/>
+                                    id="inline-full-name" type="text" placeholder={"52bfd7cf-a451-4779-8b10-b47db12cdc3f"}
+                                    {...register("numeroCmu", {required:"Veillez entrer le numero cmu"})}/>
                             </div>
+                            <span className={"text-danger"}>
+                            {errors.numeroCmu.message}
+                          </span>
                         </div>
 
                         <div className="md:flex md:items-center">
